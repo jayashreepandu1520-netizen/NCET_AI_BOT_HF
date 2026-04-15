a0fe51c4b853fbd7e489b778cd3e49abf02d42ef
jayashreepandu1520-netizen <jayashreepandu1520@gmail.com>
2026-04-15 15:34:35 +0530

    Update App.py

--- App.py
+++ App.py
@@ -22,4 +22,4 @@ if st.button("Summarize"):
       st.subheader("Summary:")
       st.success(summary[0]['summary_text'])
   else:
-    st.warning("Please enter some text to summarize."
+    st.warning("Please enter some text to summarize.")
